1f0a135a632cb91b3f33ed25feb96809a6d9c13c
Moon HyoJae <followmoonrise@gmail.com>
2020-12-31 15:51:50 +0900

    feat : 이메일, 비밀번호 검증 추가

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,21 +6,33 @@ import 'package:simple_login_flutter/state.dart';
 
 import 'dart:io';
 
+const CORRECT_EMAIL = 'test@gmail.com';
+const CORRECT_PASSWORD = '1234';
+
 class LoginPage extends StatefulWidget {
   @override
   State createState() => LoginPageState();
 }
 
 class LoginPageState extends State<LoginPage> {
+  bool _labelErrorMessage = false;
   final TextEditingController _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
   void _onLogin(BuildContext context) {
     final String email = _emailController.text;
-    final SimpleState state = Provider.of<SimpleState>(context, listen: false);
-    state.setEmail(email);
-
-    Navigator.pushReplacementNamed(context, MAIN_PAGE);
+    final String password = _passwordController.text;
+    // validate
+    if (email == CORRECT_EMAIL && password == CORRECT_PASSWORD) {
+      final SimpleState state =
+          Provider.of<SimpleState>(context, listen: false);
+      state.setEmail(email);
+      Navigator.pushReplacementNamed(context, MAIN_PAGE);
+    } else {
+      setState(() {
+        _labelErrorMessage = true;
+      });
+    }
   }
 
   void _onCancle() => exit(0);
@@ -57,6 +69,14 @@ class LoginPageState extends State<LoginPage> {
           decoration: InputDecoration(labelText: 'password'),
         ),
         SizedBox(height: 32.0),
+        Visibility(
+          child: Text(
+            '아이디 또는 비밀번호를 확인해주세요',
+            style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+          ),
+          visible: _labelErrorMessage,
+        ),
+        SizedBox(height: 8.0),
         FlatButton(
           minWidth: double.infinity,
           color: Colors.green,
